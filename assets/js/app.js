74ae880876db4ef5c75810d8b778a2a243b43f65
X-Moon-Code <52045603+X-Moon-Code@users.noreply.github.com>
2021-06-26 02:41:55 +0700

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-// Toggling a class
-
-const nav = document.querySelector("nav");
-const hamburger = document.querySelector(".hamburger");
-
-hamburger.addEventListener("click", function () {
-  nav.classList.toggle("expanded");
-});
